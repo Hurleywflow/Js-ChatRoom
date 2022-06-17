3a529f9ecfd1f2dc29bdb561ea332fa077b41b35
Hurley <86563399+Hurleywflow@users.noreply.github.com>
2022-06-17 17:21:07 +1000

    update username and messages

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,36 @@
 // get classList from the element
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+const userNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
+// add event to the new chat
+newChatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatrooms
+    .addChat(message)
+    .then(() => {
+      // clear up input form
+      newChatForm.reset();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+// add event to update userName
+userNameForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newName = userNameForm.name.value.trim();
+  chatrooms.updateUsername(newName);
+  // reset form
+  userNameForm.reset();
+  // show and hide message
+  updateMssg.innerText = `Your name is now ${newName}`;
+  setTimeout(() => {
+    updateMssg.innerText = '';
+  }, 3000);
+});
 // class UI instance
 const chatUI = new ChatUI(chatList);
 // class instance methods
